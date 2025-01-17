4c5b5710debbd0b32585a2061067287b08b41209
aiken <aikenreyes@icloud.com>
2025-01-17 21:55:46 +0000

    first commit

---
+++ generate.py
@@ -0,0 +1,60 @@
+
+import string  
+import random
+letters = list(string.ascii_letters)
+digits = list(string.digits)
+punctuation = ["!", "@", "#", "$", "%", "^", "&", "*"]
+
+characters = letters + digits + punctuation
+
+
+
+
+
+
+
+def generatePassword(howManyPasswords, passwordLength):
+    password = ""
+    while any(ele in password for ele in letters) == False and any(ele in password for ele in digits) == False and any(ele in password for ele in punctuation) == False:
+        for i in range(passwordLength):
+            password = password + random.choice(characters)
+            
+
+    
+        
+    return password
+
+# this part of code will only run if this module is executed directly
+if __name__ == "__main__": 
+    howManyPasswords = input("Enter how many passwords to generate? (default: 12): ")
+    passwordsFileName = input("Name the file to output passwords (default: passwords.txt): ")
+    passwordLength = input("How long should each password be? (default: 14): ")
+
+
+    if howManyPasswords == "":
+        howManyPasswords = 12
+    else:
+        howManyPasswords = int(howManyPasswords)
+        
+    if passwordsFileName == "":
+        passwordsFile = passwordsFile = open("passwords.txt", "a")
+    else:
+
+        passwordsFile = open(f"{passwordsFileName}.txt", "a")
+
+    if passwordLength == "":
+        passwordLength == 12
+    else:
+        passwordLength = int(passwordLength)
+
+    for i in range(howManyPasswords):
+        print(generatePassword(howManyPasswords, passwordLength))
+        passwordsFile.write(f'{generatePassword(howManyPasswords, passwordLength)}\n')
+
+    print(f"Generated {howManyPasswords} passwords, each {passwordLength} characters long and stored them in file", f"{passwordsFileName}.txt")
+    passwordsFile.close()
+
+
+
+
+
---
+++ yuh.py
@@ -0,0 +1,101 @@
+from generate import generatePassword
+
+def checkForMasterLogin():
+    masterLogin = open("masterlogin.txt", "r")
+    if masterLogin.read():
+        loginPresence = True
+        login = masterLogin.readline().split(":")
+        start(login[0], login[1])
+        masterLogin.close()
+        
+        
+    else:
+        loginPresence = False
+
+    if loginPresence == False:
+        print("Looks like it's your first time using this password manager. Please set your master login")
+        masterUsername = input("Enter master username: ")
+        masterPassword = input("Enter master password: ")
+        masterLogin = open("masterlogin.txt", "w")
+        masterLogin.write(masterUsername+":"+masterPassword)
+        masterLogin.close()
+
+        masterLogin = open("masterlogin.txt", "r")
+        login = masterLogin.readline().split(":")
+        
+        
+        start(login[0], login[1])
+
+def start(realMasterUsername, realMasterPassword):
+    welcome = print("Welcome to your password manager - press 1 to login: ")
+    
+    
+
+    valid = False
+    while valid == False:
+        masterUsername = input("Enter your username for the password manager: ")
+        masterPassword = input("Enter password: ")
+        if masterUsername == realMasterUsername and masterPassword == realMasterPassword:
+            valid = True
+            print("Welcome to your passwords.")
+            option = int(input("Press 1 to add/edit a new password or press 2 to search for a password: "))
+            if option == 1:
+                addPassword()
+            elif option == 2:
+                searchPassword()
+            
+        else:
+            print("\nIncorrect username or password, try again!\n ")
+            
+        
+
+def addPassword():
+    passwordsFile = open("passwords.txt", "a")
+    newLoginWebsite = input("Enter website name: ")
+    newUsername = input("Enter username of new login: ")
+    createUsernameMethod = input("Would you like to generate a new password? y/n: ")
+    if createUsernameMethod == "y":
+        newPassword = generatePassword(1, 12)
+        print("Generated password: ", newPassword)
+    else: 
+        newPassword = input("Enter password of new password: ")
+    passwordsFile.write("\n"+newLoginWebsite+"-"+newUsername+":"+newPassword)
+    print("Success! Added new login")
+    passwordsFile.close()
+    option = int(input("\nPress 1 to add/edit a new password or press 2 to search for a password or 3 to exit: "))
+    if option == 1:
+        addPassword()
+    elif option == 2:
+        searchPassword()
+    elif option == 3:
+        exit()
+    
+
+def searchPassword():
+    searchWebsite = input("Enter website name: ")
+    passwordsFile = open("passwords.txt", "r")
+    for i in passwordsFile:
+        websiteName = i[:i.find("-")]
+        if searchWebsite == websiteName:
+            print("Found it!")
+            withoutWebsiteName = websiteName = i[i.find("-")+1:]
+            login = withoutWebsiteName.split(":")
+            username = login[0]
+            password = login[1]
+            print("Username:", username)
+            print("Password:", password)
+    passwordsFile.close()
+    option = int(input("\nPress 1 to add/edit a new password or press 2 to search for a password or 3 to exit: "))
+    if option == 1:
+        addPassword()
+    elif option == 2:
+        searchPassword()
+    elif option == 3:
+        exit()
+
+
+checkForMasterLogin()
+
+
+
+        
